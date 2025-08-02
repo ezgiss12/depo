ff1002c85d60ac9048099eb96f40d2938d044fa3
Ezgi Sude Seker <ezgisudesekerr@gmail.com>
2025-08-02 21:06:57 +0300

    Yeni devam.html tasarımı eklendi ve düzenlemeler yapıldı

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
 
 @app.route('/')
 def home():
@@ -12,5 +12,5 @@ def devam():
     return render_template('devam.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5050))
     app.run(host='0.0.0.0', port=port, debug=True)
